200f6592a9af4589b354c0d319ce3de8c0233e47
pawan185 <Pawan.thakre3930@gmail.com>
2025-08-05 20:51:20 +0530

    Update Awards.js with new content

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -6,7 +6,7 @@ function Awards() {
                 <div className='col-6'>
                     <img src='media/images/largestBroker.svg' alt="Award 1" className="img-fluid" />
                 </div>
-                <div className='col-6 '>
+                <div className='col-6 p-5 mt-3'>
                     <h1 className='mt-5'>Largest  Stock Broker in India</h1>
                     <p>2+ millons zerodha clients  contribute  to over 15%  of all  retail order  volume in India daily  by trading and investing in.</p>
                    <div className='row'>
@@ -28,7 +28,7 @@ function Awards() {
                         </div>
                    </div>
                    
-                   <img src='media/images/pressLogos.png'  />
+                   <img src='media/images/pressLogos.png'  Style ={{width :"90%"}} />
                 </div>
 
             </div>
