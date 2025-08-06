53a8227f1f867245049058bf7758a56560a16a07
pawan185 <Pawan.thakre3930@gmail.com>
2025-08-06 23:54:56 +0530

    Update Stats.js and other frontend files

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -3,7 +3,22 @@ import React from 'react';
 
 function Stats() {
     return ( 
-        <h1>Stats</h1>
+         <div className='container '>
+            <div className='row '> 
+                <div className='col-6'>
+                  <h1>Trust with confidence</h1>  
+                    
+                </div>
+                <div className='col-6 '>
+
+                    <img src='media/images/ecosystem.png' style={{width: "75%"}}/>
+                </div>
+
+            </div>
+
+
+        </div>
+
      );
 }
 
