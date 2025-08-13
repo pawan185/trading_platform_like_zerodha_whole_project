43cfb0037a1547c226f07e4c5dcce449a4c521b3
pawan185 <Pawan.thakre3930@gmail.com>
2025-08-13 20:33:01 +0530

    Footer and openAccount part is done

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,9 +1,131 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return (  
-        <h1>Footer</h1>
-    );
+  return (
+    <footer style={{backgroundColor :"rgb(250,250,250)"}}> 
+    <div className="container border-top mt-5">
+      <div className="row mt-5">
+        <div className="col">
+          <img
+            src="media/images/logo.svg"
+            style={{ width: "40%" }}
+            alt="Logo"
+          />
+          <p> &copy; 2010 - 2025, Zerodha Broking Ltd. All rights reserved.</p>
+        </div>
+        <div className="col ">
+          <p>Account</p>
+          <a className="text-muted " style={{ textDecoration: "none" }} href="">Open demat account</a>
+          <br />
+          <a className="text-muted " style={{ textDecoration: "none" }} href="">Minor demat account</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">NRI demat account</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Commodity</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Dematerialisation</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Fund transfer</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">MTF</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Referral program</a>
+          <br />
+        </div>
+        <div className="col">
+          <p>Support</p>
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Contact us</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Support portal</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">How to file a complaint?</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Status of your complaints</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Bulletin</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Circular</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Z-Connect blog</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Downloads</a>
+          <br />
+        </div>
+        <div className="col">
+          <p>Company</p>
+          <a  className="text-muted" style={{ textDecoration: "none" }} href="">About</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Philosophy</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Press & media</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Careers</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Zerodha Cares (CSR)</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Zerodha.tech</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Open source</a>
+          <br />
+        </div>
+        <div className="col ">
+          <p>Quick links</p>
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Upcoming IPOs</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }}href="">Brokerage charges</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Market holidays</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Economic calendar</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Calculators</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Markets</a>
+          <br />
+          <a className="text-muted" style={{ textDecoration: "none" }} href="">Sectors</a>
+          <br />
+        </div>
+      </div>
+      <div className="mt-5  text-muted" style={{ fontSize: '14px' }}>
+      <p >Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.:
+      INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. –
+      SEBI Registration no.: IN-DP-431-2019 Commodity Trading through Zerodha
+      Commodities Pvt. Ltd. MCX: 46025; SEBI Registration no.: INZ000038238
+      Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars
+      Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru -
+      560078, Karnataka, India. For any complaints pertaining to securities
+      broking please write to complaints@zerodha.com, for DP related to
+      dp@zerodha.com. Please ensure you carefully read the Risk Disclosure
+      Document as prescribed by SEBI | ICF </p>
+      <p>Procedure to file a complaint on SEBI
+      SCORES: Register on SCORES portal. Mandatory details for filing complaints
+      on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits:
+      Effective Communication, Speedy redressal of the grievances </p>
+      <p>Smart Online Dispute Resolution | Grievances Redressal Mechanism </p>
+      <p>Investments in
+      securities market are subject to market risks; read all the related
+      documents carefully before investing.</p> 
+      <p>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month. </p>
+    <p>India's largest broker based on networth as per NSE. NSE broker factsheet</p>
+    <p>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.</p>
+    </div>
+    <div className="footer-graveyard-links mb-5 text-muted" style={{ textDecoration:"none" }}>
+        <ul>
+            <li><a className="text-muted" style={{ textDecoration: "none" }} href="">NSE</a></li>
+            <li><a className="text-muted" style={{ textDecoration: "none" }} href="">BSE</a></li>
+            <li><a className="text-muted" style={{ textDecoration: "none" }} href="">MCX</a></li>
+            <li><a className="text-muted" style={{ textDecoration: "none" }} href="">Terms & conditions</a></li>
+            <li><a className="text-muted" style={{ textDecoration: "none" }} href="">Policies & procedures</a></li>
+            <li><a className="text-muted" style={{ textDecoration: "none" }} href="">Privacy policy</a></li>
+            <li><a className="text-muted" style={{ textDecoration: "none" }} href="">Disclosure</a></li>
+            <li><a className="text-muted" style={{ textDecoration: "none" }} href="">For investor's attention</a></li>
+            <li><a className="text-muted" style={{ textDecoration: "none" }} href="">Investor charter</a></li>
+        </ul>
+    </div>
+    
+    </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,10 +1,60 @@
-import React from 'react';
-
+import React from "react";
 
 function Navbar() {
-    return ( 
-        <h1>Navbar</h1>
-     );
+  return (
+    <div className=" container p-2">
+      <nav class="navbar navbar-expand-lg border-bottom" style={{ backgroundColor: "#FFF" }}>
+        <a class="navbar-brand" href="#">
+          <img src="media/images/logo.svg"  style={{ width: "25%" }} alt="Logo" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item ">
+              <a class="nav-link active" href="#">
+                Signup <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link active" href="#">
+                About
+              </a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link active" href="#">
+                Products <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link active" href="#">
+                Pricing <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link active" href="#">
+                Support <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
