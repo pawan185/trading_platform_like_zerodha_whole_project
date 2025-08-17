210eb14e5777e6fc12a8ea6742f8d3c89087b69b
pawan185 <Pawan.thakre3930@gmail.com>
2025-08-17 23:35:07 +0530

    about section and products leftsection almost done

---
+++ frontend/src/landing_page/products/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Hero() {
+    return ( 
+       <div className='container text-center border-bottom mb-5 '>
+          <h1 className='mb-4'>Zerodha Products</h1>
+          <h5 className='mb-4'>Sleek, modern, and intuitive trading platforms</h5>
+          <p className='mb-5'>Check out our <a href='' style={{ textDecoration: 'none' }}>investment offerings <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
+       </div>
+     );
+}
+
+export default Hero;
