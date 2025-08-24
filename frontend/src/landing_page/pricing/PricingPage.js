19aef028e25cb1c3c6115f43f85026f524052467
pawan185 <Pawan.thakre3930@gmail.com>
2025-08-24 22:32:00 +0530

    update: today little bit of pricing part is done

--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Hero from './Hero';
+import Brokerage from './Brokerage';
+
 function PricingPage() {
-    return ( 
-       <h1>Pricing Page</h1>
-     );
+    return (
+        <>
+            <Hero />
+            <Brokerage />
+        </>
+    );
 }
 
 export default PricingPage;
