a876893a11e86d9fc62e05270ec430ff6a8e85ed
pawan185 <Pawan.thakre3930@gmail.com>
2025-08-25 16:38:23 +0530

    update : done with pricing section

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -3,8 +3,16 @@ function Brokerage() {
     return ( 
        <div className='container'>
         <div className='row'>
-          <div className='col-8'>
+          <div className='col-8 p-4'>
             <h3 className='fs-5'>Brokerage calculator</h3>
+            <ul className="text-muted fs-6"style={{textAlign: 'left',lineHeight: '2.5'}}>
+              <li>Call & Trade and RMS auto-squareoff:Additional charges of ‹50 + GST per order.</li>
+              <li>Digital contract notes will be sent via e-mail.</li>
+              <li>Physical copies of contract notes, if required, shall be charged 220 per contract note. Courier charges apply.</li>
+              <li>For NRI account (non-PIS), 0.5% or 8100 per executed order for equity (whichever is lower).</li>
+              <li>For NRI account (PIS), 0.5% or 2200 per executed order for equity (whichever is lower).</li>
+              <li>If the account is in debit balance, any order placed will be charged 840 per executed order instead of 220 per executed order.</li>
+            </ul>
           </div>
           <div className='col-4'>
             <h3 className='fs-5'>List of charges</h3>
